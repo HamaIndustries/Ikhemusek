965d6b3c2d84965204338d4d56a8811507272a57
hama <hama@hama.industries>
2026-04-28 18:53:35 -0700

    initial

---
+++ src/client/java/symbolics/division/ikhemusek/client/IkhemusekClient.java
@@ -0,0 +1,10 @@
+package symbolics.division.ikhemusek.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IkhemusekClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/symbolics/division/ikhemusek/client/IkhemusekDataGenerator.java
@@ -0,0 +1,11 @@
+package symbolics.division.ikhemusek.client;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class IkhemusekDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
