c67d6c01c332e43309ce9ebdbea7baae37d55642
hama <hama@hama.industries>
2026-04-28 19:06:27 -0700

    added it

--- src/main/java/symbolics/division/ikhemusek/Ikhemusek.java
+++ src/main/java/symbolics/division/ikhemusek/Ikhemusek.java
@@ -2,23 +2,36 @@ package symbolics.division.ikhemusek;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Ikhemusek implements ModInitializer {
+public class Ikhemusek extends Item implements ModInitializer {
 	public static final String MOD_ID = "ikhemusek";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final ResourceKey<Item> THIS = ResourceKey.create(Registries.ITEM, id("it"));
+
+	public Ikhemusek() {
+		super(new Item.Properties()
+				.setId(THIS)
+		);
+	}
+
+	public static final Identifier id(String name ) {
+		return Identifier.fromNamespaceAndPath(MOD_ID, name);
+	}
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Oh? You're approaching me?");
+		// watch this
+		Registry.register(BuiltInRegistries.ITEM, THIS, this);
 	}
 }
