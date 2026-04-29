d279f6a1ed699fb4818737701234cddf4590a175
hama <hama@hama.industries>
2026-04-28 21:07:36 -0700

    knockback

--- src/main/java/symbolics/division/ikhemusek/Ikhemusek.java
+++ src/main/java/symbolics/division/ikhemusek/Ikhemusek.java
@@ -6,12 +6,16 @@ import net.fabricmc.fabric.api.attachment.v1.AttachmentType;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.minecraft.core.Registry;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
@@ -20,7 +24,12 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
+import net.minecraft.world.item.component.UseCooldown;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.world.phys.Vec3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -56,8 +65,20 @@ public class Ikhemusek extends Item implements ModInitializer {
 	}
 
 	@Override
-	public boolean releaseUsing(ItemStack itemStack, Level level, LivingEntity entity, int remainingTime) {
-		entity.setAttached(PARRY_FRAMES, 10);
+	public boolean releaseUsing(ItemStack itemStack, Level lev, LivingEntity entity, int remainingTime) {
+		if (lev instanceof ServerLevel level) {
+			entity.setAttached(PARRY_FRAMES, 10);
+			itemStack.set(DataComponents.USE_COOLDOWN, new UseCooldown(1));
+			var hit = entity.getAttackRangeWith(itemStack).getClosesetHit(entity, 0, e -> e instanceof LivingEntity living && entity.canAttack(living));
+			if (hit instanceof BlockHitResult bhr && bhr.getType() != HitResult.Type.MISS) {
+				Vec3 eyePos = entity.getEyePosition();
+				Vec3 hitPos = bhr.getLocation();
+				Vec3 motion = eyePos.subtract(hitPos).normalize().scale(5);
+				if (entity instanceof ServerPlayer player) {
+					player.connection.send(new ClientboundSetEntityMotionPacket(player.getId(), motion));
+				}
+			}
+		}
 		return true;
 	}
 
@@ -90,8 +111,18 @@ public class Ikhemusek extends Item implements ModInitializer {
 
 	// sek? I hardly know 'er
 	public boolean ikhemu(LivingEntity entity, DamageSource source, float amount) {
-		System.out.println("awa");
 		if (entity instanceof ServerPlayer player && PERFECT(player)) {
+			var item = player.getMainHandItem();
+			if (item.is(this)
+					&& source.getDirectEntity() instanceof LivingEntity attacker
+					&& entity.getAttackRangeWith(item).getClosesetHit(entity, 0, e -> e == attacker)
+					instanceof EntityHitResult ehr
+			) {
+				Vec3 d = player.getEyePosition().subtract(attacker.getEyePosition());
+				attacker.knockback(5, d.x, d.z);
+				player.attack(entity);
+				entity.level().playSound(null, player, SoundEvents.BELL_BLOCK, SoundSource.PLAYERS, 1, 0.2f);
+			}
 			return false;
 		}
 		return true;
